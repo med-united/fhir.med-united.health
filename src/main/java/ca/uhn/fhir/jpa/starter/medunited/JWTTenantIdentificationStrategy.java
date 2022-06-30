11fb8a5678c995aa4c252727c079f215ded78e63
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2022-06-30 12:38:37 +0100

    MEDU-220 e-mail addresses from common e-mail providers should get an own tenant

--- src/main/java/ca/uhn/fhir/jpa/starter/medunited/JWTTenantIdentificationStrategy.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/medunited/JWTTenantIdentificationStrategy.java
@@ -1,7 +1,9 @@
 package ca.uhn.fhir.jpa.starter.medunited;
 
 import java.io.StringReader;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,6 +28,9 @@ public class JWTTenantIdentificationStrategy implements ITenantIdentificationStr
 
 	private static Logger log = Logger.getLogger(JWTTenantIdentificationStrategy.class.getName());
 
+	// List of common email provider domains ordered alphabetically
+	private List<String> commonEmailProviderDomains = Arrays.asList("1und1.de", "aol.com", "arcor.de", "freenet-group.de", "gmail.com", "gmx.de", "gmx.net", "hotmail.com", "icloud.com", "ionos.com", "live.com", "msn.com", "outlook.com", "t-online.de", "web.de", "yahoo.com");
+
 	@Autowired
 	private IPartitionLookupSvc myPartitionLookupSvc;
 
@@ -40,10 +45,18 @@ public class JWTTenantIdentificationStrategy implements ITenantIdentificationStr
 				String payload = new String(decoder.decode(chunks[1]));
 				JsonObject jwt = Json.createReader(new StringReader(payload)).readObject();
 				String email = jwt.getString("email");
-				String domain = email.split("@")[1].replace('.', '-');
-				
-				theRequestDetails.setTenantId(domain);
-				createPartitionIfNecessary(domain);
+				String domain = email.split("@")[1];
+	
+				String partitionName;
+				if (commonEmailProviderDomains.contains(domain)) {
+					partitionName = email.replace("@", "-").replaceAll("\\.", "-");
+				}
+				else {
+					partitionName = domain.replace('.', '-');
+				}
+
+				theRequestDetails.setTenantId(partitionName);
+				createPartitionIfNecessary(partitionName);
 				return;
 			}
 		} catch (Exception ex) {
@@ -55,18 +68,17 @@ public class JWTTenantIdentificationStrategy implements ITenantIdentificationStr
 
 	}
 
-	public void createPartitionIfNecessary(String domain) {
+	public void createPartitionIfNecessary(String partitionName) {
 		try {
-			myPartitionLookupSvc.getPartitionByName(domain);
+			myPartitionLookupSvc.getPartitionByName(partitionName);
 		} catch (ResourceNotFoundException ex) {
 			PartitionEntity partitionEntity = new PartitionEntity();
 			Integer max = myPartitionLookupSvc.listPartitions().stream().map(pe -> pe.getId()).reduce(Integer::max)
 					.orElse(0);
 			partitionEntity.setId(max + 1);
-			partitionEntity.setName(domain);
+			partitionEntity.setName(partitionName);
 			myPartitionLookupSvc.createPartition(partitionEntity);
 		}
-
 	}
 
 	@Override
